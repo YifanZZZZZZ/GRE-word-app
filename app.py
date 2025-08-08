9dde424ca21ef0d65b394a8e7328e17a8ce251e5
YifanZZZZZZ <182949398+YifanZZZZZZ@users.noreply.github.com>
2025-08-08 10:50:44 -0400

    Update Mode Selection

--- app.py
+++ app.py
@@ -159,5 +159,5 @@ def submit():
     })
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 5001))
     app.run(debug=False, host="0.0.0.0", port=port)
